997fd5d79dff3cfec6ae663112eb054caef31967
emilio <emiliogabriel000@gmail.com>
2024-10-17 12:42:43 -0300

    Criação dos objetos referente aos horários de 2024_1 e 2024_2

---
+++ extracts/extract_2024_1.py
@@ -0,0 +1,183 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from validation.penalities import calcular_fitness
+
+
+horario1 = {
+    1: {
+        'Segunda': [
+            [], [], [('Computação Ética e Sociedade', 'Cleverton Silva', None)],
+             [('Computação Ética e Sociedade', 'Cleverton Silva', None)], 
+            [('Programação I', 'Adauto Trigueiro', 'windows')],
+             [('Programação I', 'Adauto Trigueiro', 'windows')], 
+            [], [], [], [],   
+        ],
+        'Terça': [
+            [], [], [('Introdução à Computação', 'Cleverton Silva', None)],
+            [('Introdução à Computação', 'Cleverton Silva', None)],
+            [('Introdução à Computação', 'Cleverton Silva', None)],
+            [('Introdução à Computação', 'Cleverton Silva', None)],
+            [], [], [], [],  
+        ],
+        'Quarta': [
+            [], [], [('Empreendedorismo e Inovação', 'Ivaldir Honório', None)],
+            [('Empreendedorismo e Inovação', 'Ivaldir Honório', None)],
+            [('Empreendedorismo e Inovação', 'Ivaldir Honório', None)],
+            [('Empreendedorismo e Inovação', 'Ivaldir Honório', None)],
+            [('Geometria Analítica', 'Renato Silvestre', None)],
+            [('Geometria Analítica', 'Renato Silvestre', None)],
+            [('Geometria Analítica', 'Renato Silvestre', None)],
+            [('Geometria Analítica', 'Renato Silvestre', None)],
+
+        ],
+        'Quinta': [
+            [], [],  [('Programação I', 'Adauto Trigueiro', 'windows')],
+            [('Programação I', 'Adauto Trigueiro', 'windows')],
+            [('Programação I', 'Adauto Trigueiro', 'windows')],
+            [('Programação I', 'Adauto Trigueiro', 'windows')],
+            [], [], [], [], 
+        ],
+        'Sexta': [
+            [('Cálculo I', 'Janaína Barros', None)],
+            [('Cálculo I', 'Janaína Barros', None)],
+            [('Cálculo I', 'Janaína Barros', None)],
+            [('Cálculo I', 'Janaína Barros', None)],
+            [('Disciplina Curricular de Extensão 1', 'Mariana Maia', None)],
+             [('Disciplina Curricular de Extensão 1', 'Mariana Maia', None)], [], [], [], [],
+        ]
+    },
+    3: {
+        'Segunda': [
+            [], [], [('Algoritmos e Estruturas de Dados', 'Emanoel Francisco', 'linux')],
+            [('Algoritmos e Estruturas de Dados', 'Emanoel Francisco', 'linux')],
+            [('Algoritmos e Estruturas de Dados', 'Emanoel Francisco', 'linux')],
+            [('Algoritmos e Estruturas de Dados', 'Emanoel Francisco', 'linux')], [], [], [], []
+        ],
+        'Terça': [
+            [], [], [('Banco de Dados', 'Victor Santos', 'linux')],
+            [('Banco de Dados', 'Victor Santos', 'linux')],
+            [('Banco de Dados', 'Victor Santos', 'linux')],
+            [('Banco de Dados', 'Victor Santos', 'linux')],
+            [], [], [], [], 
+        ],
+        'Quarta': [
+            [('Disciplina Curricular de Extensão 3', 'Ewerton Mendonça', None)],
+            [('Disciplina Curricular de Extensão 3', 'Victor Santos', None)],
+            [('Programação III', 'Emanoel Francisco', 'linux')],
+            [('Programação III', 'Emanoel Francisco', 'linux')],
+            [('Programação III', 'Emanoel Francisco', 'linux')],
+            [('Programação III', 'Emanoel Francisco', 'linux')],
+            [('Estatística I', 'Milton Perceus', None)],
+            [('Estatística I', 'Milton Perceus', None)],
+            [('Estatística I', 'Milton Perceus', None)],
+            [('Estatística I', 'Milton Perceus', None)],
+
+        ],
+        'Quinta': [
+            [('Projeto de Software', 'Ewerton Mendonça', None)],
+            [('Projeto de Software', 'Ewerton Mendonça', None)],
+            [('Projeto de Software', 'Ewerton Mendonça', None)],
+            [('Projeto de Software', 'Ewerton Mendonça', None)],
+            [('Programação III', 'Emanoel Francisco', 'linux')],
+            [('Programação III', 'Emanoel Francisco', 'linux')], [], [], [], [],
+        ],
+        'Sexta': [
+            [], [],
+            [('Engenharia de Requisitos', 'Mariana Maia', None)],
+            [('Engenharia de Requisitos', 'Mariana Maia', None)],
+            [('Engenharia de Requisitos', 'Mariana Maia', None)],
+            [('Engenharia de Requisitos', 'Mariana Maia', None)],
+             [], [], [], [], 
+        ]
+    },
+    5: {
+        'Segunda': [
+            [('Disciplina Curricular de Extensão 5', 'Cleverton Silva', None)],
+            [('Disciplina Curricular de Extensão 5', 'Cleverton Silva', None)],
+            [('Verificação e Validação de Sistemas', 'Brunno Wagner', None)],
+            [('Verificação e Validação de Sistemas', 'Brunno Wagner', None)],
+            [('Verificação e Validação de Sistemas', 'Brunno Wagner', None)],
+            [('Verificação e Validação de Sistemas', 'Brunno Wagner', None)], [], [], [], [],
+        ],
+        'Terça': [
+            [], [],
+            [('Gerência de Projetos', 'Aêda Monalliza', None)],
+            [('Gerência de Projetos', 'Aêda Monalliza', None)],
+            [('Gerência de Projetos', 'Aêda Monalliza', None)],
+            [('Gerência de Projetos', 'Aêda Monalliza', None)],
+            [('Redes de Computadores', 'Cleiton Martins', 'windows')],
+            [('Redes de Computadores', 'Cleiton Martins', 'windows')],
+            [('Redes de Computadores', 'Cleiton Martins', 'windows')],
+            [('Redes de Computadores', 'Cleiton Martins', 'windows')]
+        ],
+        'Quarta': [
+            [('Programação para Dispositivos Móveis', 'Elisson Rocha', 'windows')],
+            [('Programação para Dispositivos Móveis', 'Elisson Rocha', 'windows')],
+            [('Programação para Dispositivos Móveis', 'Elisson Rocha', 'windows')],
+            [('Programação para Dispositivos Móveis', 'Elisson Rocha', 'windows')],
+            [], [], [], [], [], []
+        ],
+        'Quinta': [
+            [('Projeto I', 'Marcello Mendonça', None)],
+            [('Projeto I', 'Marcello Mendonça', None)],
+            [('Projeto I', 'Marcello Mendonça', None)],
+            [('Projeto I', 'Marcello Mendonça', None)],
+            [], [], [], [], [], [], 
+        ],
+        'Sexta': [
+            [('Paradigmas de Linguagens de Programação', 'Eraylson Galdino', 'windows')],
+            [('Paradigmas de Linguagens de Programação', 'Eraylson Galdino', 'windows')],
+            [('Paradigmas de Linguagens de Programação', 'Eraylson Galdino', 'windows')],
+            [('Paradigmas de Linguagens de Programação', 'Eraylson Galdino', 'windows')],
+            [], [], [], [], [], []
+        ]
+    },
+    7: {
+        'Segunda': [
+            [('Segurança de Sistemas', 'Cleiton Martins', 'windows')],
+            [('Segurança de Sistemas', 'Cleiton Martins', 'windows')],
+            [('Segurança de Sistemas', 'Cleiton Martins', 'windows')],
+            [('Segurança de Sistemas', 'Cleiton Martins', 'windows')],
+            [('Disciplina Curricular de Extensão 7', 'Cleverton Silva', 'windows')],
+            [('Disciplina Curricular de Extensão 7', 'Cleverton Silva', 'windows')],
+
+            [], [], [], [], 
+        ],
+        'Terça': [
+            [('Qualidade de Software', 'Brunno Wagner', None)],
+            [('Qualidade de Software', 'Brunno Wagner', None)],
+            [('Qualidade de Software', 'Brunno Wagner', None)],
+            [('Qualidade de Software', 'Brunno Wagner', None)],
+            [], [], [], [], [], [], 
+        ],
+        'Quarta': [
+            [], [],
+            [('Cadeira Eletiva', 'Ewerton Mendonça', None)],
+            [('Cadeira Eletiva', 'Ewerton Mendonça', None)],
+            [('Cadeira Eletiva', 'Ewerton Mendonça', None)],
+            [('Cadeira Eletiva', 'Ewerton Mendonça', None)], [], [], [], [],
+        ],
+        'Quinta': [
+            [('Seminário em Engenharia de Software I', 'Marcello Mendonça', None)],
+            [('Seminário em Engenharia de Software I', 'Marcello Mendonça', None)],
+            [('Projeto II', 'Mariana Maia', 'windows')],
+            [('Projeto II', 'Mariana Maia', 'windows')],
+            [('Projeto II', 'Mariana Maia', 'windows')],
+            [('Projeto II', 'Mariana Maia', 'windows')], [], [], [], [],
+        ],
+        'Sexta': [
+            [], [],
+            [('Cadeira Eletiva', 'Ariane Nunes', 'linux')],
+            [('Cadeira Eletiva', 'Ariane Nunes', 'linux')],
+            [('Cadeira Eletiva', 'Ariane Nunes', 'linux')],
+            [('Cadeira Eletiva', 'Ariane Nunes', 'linux')],
+            [], [], [], [], 
+        ]
+    }
+}
+
+
+fitness_1 = calcular_fitness(horario1)
+print(fitness_1)
---
+++ extracts/extract_2024_2.py
@@ -0,0 +1,199 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from validation.penalities import calcular_fitness
+
+
+horario_2 = {
+    2: { 
+        'Segunda': [
+            [],  [], 
+            [('Processos de Software', 'Brunno Wagner', None)],
+            [('Processos de Software', 'Brunno Wagner', None)],
+            [('Processos de Software', 'Brunno Wagner', None)],
+            [('Processos de Software', 'Brunno Wagner', None)],  
+            [], [], [],  [],
+        ],
+        'Terça': [
+            [('Cálculo II', 'Paulo Júnior', None)],
+            [('Cálculo II', 'Paulo Júnior', None)],
+            [('Cálculo II', 'Paulo Júnior', None)],
+            [('Cálculo II', 'Paulo Júnior', None)], 
+            [('Programação II', 'Emanoel Francisco', None)],
+            [('Programação II', 'Emanoel Francisco', None)],   
+            [], [],  [],  [],   
+        ],
+        'Quarta': [
+            [('Álgebra Linear', 'Renato Silvestre', None)],
+            [('Álgebra Linear', 'Renato Silvestre', None)],  
+            [('Metodologia Científica', 'Ivaldir Honório', None)],
+            [('Metodologia Científica', 'Ivaldir Honório', None)],
+            [('Metodologia Científica', 'Ivaldir Honório', None)],
+            [('Metodologia Científica', 'Ivaldir Honório', None)], 
+            [('Disciplina Curricular de Extensão 2', 'Ivaldir Honório', None)],
+            [('Disciplina Curricular de Extensão 2', 'Ivaldir Honório', None)],
+            [('Disciplina Curricular de Extensão 2', 'Ivaldir Honório', None)], 
+            [], 
+        ],
+        'Quinta': [
+            [('Álgebra Linear', 'Renato Silvestre', None)],
+            [('Álgebra Linear', 'Renato Silvestre', None)], 
+            [('Matemática Discreta', 'Geovany Fernandes', None)],
+            [('Matemática Discreta', 'Geovany Fernandes', None)], 
+            [('Matemática Discreta', 'Geovany Fernandes', None)], 
+            [('Matemática Discreta', 'Geovany Fernandes', None)],  
+            [], [], [], [],
+        ],
+        'Sexta': [
+            [],  [],  
+            [('Programação II', 'Emanoel Francisco', None)],
+            [('Programação II', 'Emanoel Francisco', None)],
+            [('Programação II', 'Emanoel Francisco', None)],
+            [('Programação II', 'Emanoel Francisco', None)],  
+            [],  [],  [],  [],  
+        ]
+    },
+    4: {  
+        'Segunda': [
+            [], [], 
+            [('Organização e Arquitetura de Computadores', 'Cleiton Martins', None)],
+            [('Organização e Arquitetura de Computadores', 'Cleiton Martins', None)],
+            [('Organização e Arquitetura de Computadores', 'Cleiton Martins', None)],
+            [('Organização e Arquitetura de Computadores', 'Cleiton Martins', None)],  
+            [], [], [], [], 
+        ],
+        'Terça': [
+            [('Teoria da Computação', 'Brunno Wagner', None)],
+            [('Teoria da Computação', 'Brunno Wagner', None)],
+            [('Teoria da Computação', 'Brunno Wagner', None)],
+            [('Teoria da Computação', 'Brunno Wagner', None)],  
+            [('Disciplina Curricular de Extensão 4', 'Cleverton Silva', None)],
+            [('Disciplina Curricular de Extensão 4', 'Cleverton Silva', None)],    
+            [('Gerência de Configuração', 'Ewerton Mendonça', None)],
+            [('Gerência de Configuração', 'Ewerton Mendonça', None)],
+            [('Gerência de Configuração', 'Ewerton Mendonça', None)],
+            [('Gerência de Configuração', 'Ewerton Mendonça', None)],  
+        ],
+        'Quarta': [
+            [('Padrões de Projeto', 'Ewerton Mendonça', None)],
+            [('Padrões de Projeto', 'Ewerton Mendonça', None)],
+            [('Padrões de Projeto', 'Ewerton Mendonça', None)],
+            [('Padrões de Projeto', 'Ewerton Mendonça', None)], 
+            [], [],  
+            [('Estatística II', 'Milton Perceus', None)],
+            [('Estatística II', 'Milton Perceus', None)],
+            [('Estatística II', 'Milton Perceus', None)],
+            [('Estatística II', 'Milton Perceus', None)],  
+        ],
+        'Quinta': [
+            [], [], [], [], [], [],
+            [('Programação para Web', 'Elisson Rocha', None)],
+            [('Programação para Web', 'Elisson Rocha', None)],
+            [('Programação para Web', 'Elisson Rocha', None)],
+            [('Programação para Web', 'Elisson Rocha', None)],  
+        ],
+        'Sexta': [
+            [],  [],  [],  [],  [],  [],  [],  [],  [],   [], 
+            
+        ]
+    },
+    6: {  
+        'Segunda': [
+            [], [],  
+            [('Engenharia de Software Experimental', 'Adauto Trigueiro', None)],
+            [('Engenharia de Software Experimental', 'Adauto Trigueiro', None)],
+            [('Engenharia de Software Experimental', 'Adauto Trigueiro', None)],
+            [('Engenharia de Software Experimental', 'Adauto Trigueiro', None)],  
+            [('Arquitetura de Software', 'Brunno Wagner', None)],
+            [('Arquitetura de Software', 'Brunno Wagner', None)],
+            [('Arquitetura de Software', 'Brunno Wagner', None)],
+            [('Arquitetura de Software', 'Brunno Wagner', None)], 
+        ],
+        'Terça': [
+            [], [], 
+            [('Sistemas Operacionais', 'Cleiton Martins', None)],
+            [('Sistemas Operacionais', 'Cleiton Martins', None)],
+            [('Sistemas Operacionais', 'Cleiton Martins', None)],
+            [('Sistemas Operacionais', 'Cleiton Martins', None)],  
+            [],  [],  [],  [],  
+        ],
+        'Quarta': [
+            [('Inteligência Artificial', 'Eraylson Galdino', None)],
+            [('Inteligência Artificial', 'Eraylson Galdino', None)],
+            [('Inteligência Artificial', 'Eraylson Galdino', None)],
+            [('Inteligência Artificial', 'Eraylson Galdino', None)],  
+            [('Disciplina Curricular de Extensão 6', 'Eraylson Galdino', None)],
+            [('Disciplina Curricular de Extensão 6', 'Eraylson Galdino', None)], 
+            [],  [],  [],  [],  
+        ],
+        'Quinta': [
+            [('Integração de Sistemas', 'Elisson Rocha', None)],
+            [('Integração de Sistemas', 'Elisson Rocha', None)],
+            [('Integração de Sistemas', 'Elisson Rocha', None)],
+            [('Integração de Sistemas', 'Elisson Rocha', None)],  
+            [], [], [], [], [], [],  
+        ],
+        'Sexta': [
+            [], [], 
+            [('Interação Humano-Computador', 'Ariane Nunes', None)],
+            [('Interação Humano-Computador', 'Ariane Nunes', None)],
+            [('Interação Humano-Computador', 'Ariane Nunes', None)],
+            [('Interação Humano-Computador', 'Ariane Nunes', None)], 
+            [], [], [], [], 
+        ]
+    },
+    8: {  
+        'Segunda': [
+            [],  [], 
+            [('Manutenção e Evolução de Software', 'Victor Santos', None)],
+            [('Manutenção e Evolução de Software', 'Victor Santos', None)],
+            [('Manutenção e Evolução de Software', 'Victor Santos', None)],
+            [('Manutenção e Evolução de Software', 'Victor Santos', None)],  
+            [], [], [], [], 
+        ],
+        'Terça': [
+            [], [],
+            [('Computação Gráfica e Sistemas Multimídia', 'Ewerton Mendonça', None)],
+            [('Computação Gráfica e Sistemas Multimídia', 'Ewerton Mendonça', None)],
+            [('Computação Gráfica e Sistemas Multimídia', 'Ewerton Mendonça', None)],
+            [('Computação Gráfica e Sistemas Multimídia', 'Ewerton Mendonça', None)],  
+            [('Aprendizagem de Máquina e Reconhecimento de Padrões', 'Victor Santos', None)],
+            [('Aprendizagem de Máquina e Reconhecimento de Padrões', 'Victor Santos', None)],
+            [('Aprendizagem de Máquina e Reconhecimento de Padrões', 'Victor Santos', None)],
+            [('Aprendizagem de Máquina e Reconhecimento de Padrões', 'Victor Santos', None)],  
+        ],
+        'Quarta': [
+            [], [],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas I', 'Victor Santos', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas I', 'Victor Santos', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas I', 'Victor Santos', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas I', 'Victor Santos', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas I', 'Victor Santos', None)],  
+            [('Cadeira Eletiva', 'Elisson Rocha', None)],
+            [('Cadeira Eletiva', 'Elisson Rocha', None)],
+            [('Cadeira Eletiva', 'Elisson Rocha', None)],
+            [('Cadeira Eletiva', 'Elisson Rocha', None)],  
+        ],
+        'Quinta': [
+            [('Disciplina Curricular de Extensão 8', 'Mariana Maia', None)],
+            [('Disciplina Curricular de Extensão 8', 'Mariana Maia', None)], 
+            [],  [],  [],  [], 
+            [('Seminário em Engenharia de Software II', 'Marcello Mendonça', None)],
+            [('Seminário em Engenharia de Software II', 'Marcello Mendonça', None)],
+            [],  [], 
+        ],
+        'Sexta': [
+            [], [],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas IV', 'Mariana Maia', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas IV', 'Mariana Maia', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas IV', 'Mariana Maia', None)],
+            [('Tópicos Avançados em Engenharia de Software e Sistemas IV', 'Mariana Maia', None)],
+            [], [], [], [],  
+        ]
+    }
+}
+
+
+fitness_1 = calcular_fitness(horario_2)
+print(fitness_1)
